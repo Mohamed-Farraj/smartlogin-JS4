9d8cf673d09a30f3fa8553d3aa0651bf9d066121
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2024-06-13 05:07:49 +0300

    another try

--- script/scriptlogin.js
+++ script/scriptlogin.js
@@ -17,7 +17,7 @@ document.getElementById("form").addEventListener("submit", function (e) {
         passwordInput.value = "";
         err = false;
         localStorage.setItem('un',accounts[i].username);
-        window.location.href = "../home.html";
+        window.location.href = "./home.html";
         break;
       }
     }
